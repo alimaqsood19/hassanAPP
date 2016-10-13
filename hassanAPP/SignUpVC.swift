e2f21a1e47edf1c0e451d97e42b2f126adc5563b
Ali Maqsood <ali-maqsood@Hotmail.com>
2016-10-13 00:58:47 -0400

    Finished

--- hassanAPP/SignUpVC.swift
+++ hassanAPP/SignUpVC.swift
@@ -25,10 +25,6 @@ class SignUpVC: UIViewController {
         
         webView.loadRequest(requestOBJ as URLRequest)
         
-        
-        
-
-
     }
 
 
---
+++ hassanAPP/WebsiteVC.swift
@@ -0,0 +1,21 @@
+//
+//  WebsiteVC.swift
+//  hassanAPP
+//
+//  Created by Ambar Maqsood on 2016-10-12.
+//  Copyright © 2016 Ambar Maqsood. All rights reserved.
+//
+
+import UIKit
+
+class WebsiteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    
+
+}
