8ad3f91d780a1315309d66d0ed4e3139db7cce96
Ali Maqsood <ali-maqsood@Hotmail.com>
2016-11-02 00:28:43 -0400

    Added the Firebase pod frameworks and created SignUPAuthVC for social media

---
+++ hassanAPP/SignUpAuthVC.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpAuthVC.swift
+//  hassanAPP
+//
+//  Created by Ambar Maqsood on 2016-11-02.
+//  Copyright © 2016 Ambar Maqsood. All rights reserved.
+//
+
+import UIKit
+
+class SignUpAuthVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
